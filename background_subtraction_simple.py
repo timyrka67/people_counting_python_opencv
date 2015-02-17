af499f05723112279f8d4a20a01097926c0c2218
Timur Aibusehev <timyrka67@gmail.com>
2015-02-17 17:44:03 +0300

    All u

---
+++ background_subtraction_simple.py
@@ -0,0 +1,22 @@
+__author__ = 'timur'
+import numpy as np
+import cv2
+import cv2.cv as cv
+cap = cv2.VideoCapture(0)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+fgbg = cv2.BackgroundSubtractorMOG()
+
+while(1):
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    cv2.imshow('frame',fgmask)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
